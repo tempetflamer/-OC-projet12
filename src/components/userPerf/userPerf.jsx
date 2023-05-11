f8266e1d93a0eb7cd49ff647aa2014eb3c4b0ae4
tempet <tempetflamer@gmail.com>
2023-05-12 00:34:56 +0200

    update change the retrieving data method for useState and useEffect

--- src/components/userPerf/userPerf.jsx
+++ src/components/userPerf/userPerf.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import UserKeyData from '../UserKeyData/UserKeyData'
 import useUserInfos from '../../hooks/useUserInfos.jsx'
@@ -7,19 +8,30 @@ import iconChicken from '../../assets/chicken.svg'
 import iconApple from '../../assets/apple.svg'
 import iconBurger from '../../assets/cheeseburger.svg'
 
-/** Display of the section containing all userKeyDatas
- * @return {JSX}
+/**
+ * Display of the section containing all userKeyDatas
  */
 export default function userPerf() {
   const { userID } = useParams()
-  const userInfo = useUserInfos(userID)
+  const [keyData, setKeyData] = useState()
+  const { data, isLoading, error } = useUserInfos(userID)
 
+  useEffect(() => {
+    if (!isLoading && !error && data.id) {
+      const keyData = data.keyData
+      setKeyData(keyData)
+    }
+  }, [data, isLoading, error])
+
+  if (!keyData) {
+    return ''
+  }
   return (
     <section className="perfs">
-      <UserKeyData userInfo={userInfo?.data?.keyData?.calorieCount} unite="Calories" abbUnite="kCal" color="red" icon={iconEnergy} />
-      <UserKeyData userInfo={userInfo?.data?.keyData?.proteinCount} unite="Proteines" abbUnite="g" color="blue" icon={iconChicken} />
-      <UserKeyData userInfo={userInfo?.data?.keyData?.carbohydrateCount} unite="Glucides" abbUnite="g" color="yellow" icon={iconApple} />
-      <UserKeyData userInfo={userInfo?.data?.keyData?.lipidCount} unite="Lipides" abbUnite="g" color="pink" icon={iconBurger} />
+      <UserKeyData userInfo={keyData.calorieCount} unite="Calories" abbUnite="kCal" color="red" icon={iconEnergy} />
+      <UserKeyData userInfo={keyData.proteinCount} unite="Proteines" abbUnite="g" color="blue" icon={iconChicken} />
+      <UserKeyData userInfo={keyData.carbohydrateCount} unite="Glucides" abbUnite="g" color="yellow" icon={iconApple} />
+      <UserKeyData userInfo={keyData.lipidCount} unite="Lipides" abbUnite="g" color="pink" icon={iconBurger} />
     </section>
   )
 }
